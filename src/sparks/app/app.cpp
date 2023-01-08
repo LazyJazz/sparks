2d7a3d812e833a311b31611dc0e9481c574be0f0
LazyJazz <zijian.lv@hotmail.com>
2023-01-08 12:40:37 +0800

    Scene descriptor file

--- src/sparks/app/app.cpp
+++ src/sparks/app/app.cpp
@@ -425,8 +425,8 @@ void App::UpdateImGui() {
       ImGui::Text("Material");
       ImGui::Separator();
       static int current_item = 0;
-      std::vector<const char *> material_types = {"Lambertian", "Specular",
-                                                  "Transmissive", "Principled"};
+      std::vector<const char *> material_types = {
+          "Lambertian", "Specular", "Transmissive", "Principled", "Emission"};
       Material &material = scene.GetEntity(selected_entity_id_).GetMaterial();
       reset_accumulation_ |=
           ImGui::Combo("Type", reinterpret_cast<int *>(&material.material_type),
