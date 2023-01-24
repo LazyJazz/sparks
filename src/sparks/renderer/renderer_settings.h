f031b44a7ec65d0550524717b79b7a6028571f28
LazyJazz <zijian.lv@hotmail.com>
2023-01-24 23:10:45 +0800

    Support alpha shadow

--- src/sparks/renderer/renderer_settings.h
+++ src/sparks/renderer/renderer_settings.h
@@ -6,5 +6,7 @@ struct RendererSettings {
   int num_bounces{10};
   float envmap_scale{1.0f};
   bool enable_mis{true};
+  bool enable_alpha_shadow{true};
+  int output_selection{0};
 };
 }  // namespace sparks
