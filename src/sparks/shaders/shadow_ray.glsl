7334b4815f835f7bfb31f6f8a684e78ea60ecd64
LazyJazz <zijian.lv@hotmail.com>
2023-02-06 11:02:41 +0800

    Fix alpha shadow artifact

--- src/sparks/shaders/shadow_ray.glsl
+++ src/sparks/shaders/shadow_ray.glsl
@@ -6,20 +6,29 @@ float ShadowRay(vec3 origin, vec3 direction, float dist) {
   rayQueryEXT rq;
   if (global_uniform_object.enable_alpha_shadow) {
     float r1 = RandomFloat();
-    rayQueryInitializeEXT(rq, scene, gl_RayFlagsNoOpaqueEXT, 0xFF, origin,
-                          length(origin) * 1e-3, direction, dist * 0.999);
+    float t_max = dist * 0.999;
+    while (true) {
+      rayQueryInitializeEXT(rq, scene, gl_RayFlagsOpaqueEXT, 0xFF, origin,
+                            length(origin) * 1e-4, direction, t_max);
+      rayQueryProceedEXT(rq);
 
-    // Traverse the acceleration structure and store information about the first
-    // intersection (if any)
-    while (rayQueryProceedEXT(rq)) {
-      uint type = rayQueryGetIntersectionTypeEXT(rq, false);
-      float alpha =
-          materials[rayQueryGetIntersectionInstanceCustomIndexEXT(rq, false)]
-              .alpha;
-      if (r1 < 1.0 - alpha) {
-        r1 /= 1.0 - alpha;
+      // Traverse the acceleration structure and store information about the
+      // first intersection (if any)
+      uint type = rayQueryGetIntersectionTypeEXT(rq, true);
+      if (type != gl_RayQueryCommittedIntersectionNoneEXT) {
+        float alpha =
+            materials[rayQueryGetIntersectionInstanceCustomIndexEXT(rq, true)]
+                .alpha;
+        if (r1 < 1.0 - alpha) {
+          r1 /= 1.0 - alpha;
+          float t = rayQueryGetIntersectionTEXT(rq, true);
+          origin = origin + direction * t;
+          t_max -= t;
+        } else {
+          return 0.0;
+        }
       } else {
-        return 0.0;
+        return 1.0;
       }
     }
   } else {
